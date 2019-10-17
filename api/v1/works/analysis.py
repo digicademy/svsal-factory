385b68f3631d97ea2b428beff58480e74ff9897b
David Glueck <33424164+dvglc@users.noreply.github.com>
2019-10-17 13:41:27 +0200

    Pre-compile xpaths for element types

--- api/v1/works/analysis.py
+++ api/v1/works/analysis.py
@@ -1,38 +1,58 @@
 from lxml import etree
+from api.v1.works.xutils import flatten, xml_ns, is_element
 
 
-ns = {'xml': 'http://www.w3.org/XML/1998/namespace',
-      'tei': 'http://www.tei-c.org/ns/1.0',
-      'xi': 'http://www.w3.org/2001/XInclude'}
 
 # Element type definitions
-structural_elems = \
-    ['self::tei:div[@type != "work_part"]',
-     'self::tei:back',
-     'self::tei:front',
-     'self::tei:text[@type = "work_volume"]']
-main_elems = \
-    ['self::tei:p',
-     'self::tei:signed',
-     'self::tei:head[not(ancestor::tei:list)]',
-     'self::tei:titlePage',
-     'self::tei:lg',
-     'self::tei:label[@place != "margin"]',
-     'self::tei:argument[not(ancestor::tei:list)]',
-     'self::tei:table']
-marginal_elems = \
-    ['self::tei:note[@place = "margin"]',
-     'self::tei:label[@place = "margin"]']
-page_elems = \
-    ['self::tei:pb[not(@sameAs or @corresp)]']
-anchor_elems = \
-    ['self::tei:milestone[@unit != "other"]']
-    # TODO: inline labels
-list_elems = \
-    ['self::tei:list',
-     'self::tei:item',
-     'self::tei:head[ancestor::tei:list]',
-     'self::tei:argument[ancestor::tei:list]']
+structural_elem_xpath = \
+    'boolean(' + \
+    'self::tei:div[@type != "work_part"] or ' + \
+    'self::tei:back or ' + \
+    'self::tei:front or ' + \
+    'self::tei:text[@type = "work_volume"]' + \
+    ')'
+main_elem_xpath = \
+    'boolean(' + \
+    'self::tei:p or ' + \
+    'self::tei:signed or ' + \
+    'self::tei:head[not(ancestor::tei:list)] or ' + \
+    'self::tei:titlePage or ' + \
+    'self::tei:lg or ' + \
+    'self::tei:label[@place != "margin"] or ' + \
+    'self::tei:argument[not(ancestor::tei:list)] or ' + \
+    'self::tei:table' \
+    ')'
+marginal_elem_xpath = \
+    'boolean(' \
+    'self::tei:note[@place = "margin"] or ' \
+    'self::tei:label[@place = "margin"]' \
+    ')'
+page_elem_xpath = \
+    'boolean(' \
+    'self::tei:pb[not(@sameAs or @corresp)]' \
+    ')'
+anchor_elem_xpath = \
+    'boolean(' \
+    'self::tei:milestone[@unit != "other"]' \
+    ')' # TODO: inline labels
+list_elem_xpath = \
+    'boolean(' \
+    'self::tei:list or ' \
+    'self::tei:item or ' \
+    'self::tei:head[ancestor::tei:list] or ' \
+    'self::tei:argument[ancestor::tei:list]' \
+    ')'
+
+
+# XPath classes / functions
+is_structural_elem = etree.XPath(structural_elem_xpath, namespaces=xml_ns)
+main_ancestors_xpath = 'not(ancestor::*[' + main_elem_xpath + ' or ' + marginal_elem_xpath + ' or ' + list_elem_xpath + '])'
+is_main_elem = etree.XPath(main_elem_xpath + ' and ' + main_ancestors_xpath, namespaces=xml_ns)
+is_marginal_elem = etree.XPath(marginal_elem_xpath, namespaces=xml_ns)
+is_anchor_elem = etree.XPath(anchor_elem_xpath, namespaces=xml_ns)
+is_page_elem = etree.XPath(page_elem_xpath, namespaces=xml_ns)
+list_ancestors_xpath = 'not(ancestor::*[' + main_elem_xpath + ' or ' + marginal_elem_xpath + '])'
+is_list_elem = etree.XPath(list_elem_xpath + ' and ' + list_ancestors_xpath, namespaces=xml_ns)
 
 
 # TODO
@@ -49,17 +69,23 @@ def extract_metadata(wid, tei_header):
 
 
 def extract_text_structure(wid, node):
-    if etree.iselement(node):
-        if get_elem_type(node) and len(node.xpath('./@xml:id')):
-            #print('Found index node: ' + node.tag)
-            #sal_node = etree.Element('sal_node')
-            #sal_children = ...
-            #return sal_node
-            for child in node:
-                extract_text_structure(wid, child)
+    if is_element(node):
+        elem_type = get_elem_type(node)
+        if len(node.xpath('./@xml:id')) and elem_type:
+            print('Processing ' + elem_type + ' element: ' + node.tag + ' (' + node.xpath('./@xml:id')[0] + ')')
+            sal_node = etree.Element('sal_node')
+            sal_node.set('n', node.xpath('./@xml:id')[0])
+            sal_node.set('type', elem_type)
+            sal_children = flatten([extract_text_structure(wid, child) for child in node])
+            for sal_child in sal_children:
+                if etree.iselement(sal_child):
+                    print('Found sal_child!')
+                    sal_node.append(sal_child)
+                elif isinstance(sal_child, list):
+                    print('Found list: ' + '; '.join(sal_child))
+            return sal_node
         else:
-            for child in node:
-                extract_text_structure(wid, child)
+            return [extract_text_structure(wid, child) for child in node]
     else:
         pass
 
@@ -69,83 +95,23 @@ def get_elem_type(elem):
     Determines the type of an indexable element. If the element is not indexable, an empty string is returned.
     """
     if is_structural_elem(elem):
-        print('Found structural element: ' + elem.tag)
+        #print('Found structural element: ' + elem.tag)
         return 'structural'
     elif is_main_elem(elem):
-        print('Found main element: ' + elem.tag)
+        #print('Found main element: ' + elem.tag)
         return 'main'
     elif is_marginal_elem(elem):
-        print('Found marginal element: ' + elem.tag)
+        #print('Found marginal element: ' + elem.tag)
         return 'marginal'
     elif is_page_elem(elem):
-        print('Found page element: ' + elem.tag)
+        #print('Found page element: ' + elem.tag)
         return 'page'
     elif is_anchor_elem(elem):
-        print('Found anchor element: ' + elem.tag)
+        #print('Found anchor element: ' + elem.tag)
         return 'anchor'
     elif is_list_elem(elem):
-        print('Found list element: ' + elem.tag)
+        #print('Found list element: ' + elem.tag)
         return 'list'
     else:
         return ''
-
-
-
-def is_structural_elem(elem):
-    for expr in structural_elems:
-        if len(elem.xpath(expr, namespaces=ns)):
-            return True
-    return False
-
-
-def is_main_elem(elem):
-    if check_main_ancestors(elem):
-        for expr in main_elems:
-            if len(elem.xpath(expr, namespaces=ns)):
-                return True
-    return False
-
-
-def check_main_ancestors(elem):
-    for ancestor in elem.xpath('ancestor::*'):
-        if is_main_elem(ancestor) or is_marginal_elem(ancestor) or is_list_elem(ancestor):
-            # use 'self::tei:list' instead of 'is_list_elem(ancestor)' in case of endless loop
-            return False
-    return True
-
-
-def is_marginal_elem(elem):
-    for expr in marginal_elems:
-        if len(elem.xpath(expr, namespaces=ns)):
-            return True
-    return False
-
-
-def is_anchor_elem(elem):
-    for expr in anchor_elems:
-        if len(elem.xpath(expr, namespaces=ns)):
-            return True
-    return False
-
-
-def is_page_elem(elem):
-    for expr in page_elems:
-        if len(elem.xpath(expr, namespaces=ns)):
-            return True
-    return False
-
-
-def is_list_elem(elem):
-    if check_list_ancestors(elem):
-        for expr in list_elems:
-            if len(elem.xpath(expr, namespaces=ns)):
-                return True
-    return False
-
-
-def check_list_ancestors(elem):
-    for ancestor in elem.xpath('ancestor::*'):
-        if is_main_elem(ancestor) or is_marginal_elem(ancestor):
-            return False
-    return True
 
